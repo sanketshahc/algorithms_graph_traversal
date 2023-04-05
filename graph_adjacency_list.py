c1a7aba25ef4b752c81c06440558b08fd71341a2
sanketshahc <sanket.shah.c@gmail.com>
2023-04-05 17:34:13 -0400

    tie in

--- graph_adjacency_list.py
+++ graph_adjacency_list.py
@@ -6,6 +6,9 @@
 # means that it's represented as a map from each node to a list of it's
 # respective adjacent nodes.
 
+# import random
+
+
 class Graph:
     def __init__(self):
         # DO NOT EDIT THIS CONSTRUCTOR
@@ -65,5 +68,42 @@ class Graph:
                         ls[n], ls[n + 1] = ls[n + 1], ls[n]
 
         return ls or []
+
+
+# Must uncomment module import above
+def rand_graph():
+    """
+    :returns: dict, ls
+
+    construct psuedo random directed graphs of 26 nodes. returns nodes dict and edges list
+    """
+
+    edges = 8
+    weights = 20
+
+    e = int(random.normalvariate(edges, 2))
+    r = random.randint
+    possible_neighbors = [
+        [chr(i) for i in range(ord('a'), ord('h') + 1)],
+        [chr(i) for i in range(ord('e'), ord('m') + 1)],
+        [chr(i) for i in range(ord('k'), ord('s') + 1)],
+        [chr(i) for i in range(ord('p'), ord('z') + 1)],
+    ]
+    dict = {}
+    ls = []
+    for i in possible_neighbors:
+        for j in i:
+            p = i.copy()
+            for k in range(0, r(3, min(len(i) - 3, e))):
+                del (p[r(0, len(p) - 1)])
+                p.remove(j) if j in p else None
+                n = [(q, r(1, weights)) for q in p]
+            dict[f'{j}'] = n
+    for i, j in dict.items():
+        ls.extend([(i, n, w) for n, w in j])
+
+    return dict, ls
+
+
 
 
